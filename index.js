e7579e4e3a917f9f3fb24606827c3db79eb1712d
matheusrenner22 <matheusrennerdev@outlook.com>
2024-06-27 04:45:24 -0300

    troquei de factory function para constructor function

--- index.js
+++ index.js
@@ -1,98 +1,96 @@
-function criaCalculadora() {
-    return {
+function Calculadora() {
+    const display = document.querySelector('.display');
 
-        display: document.querySelector('.display'),
+    const btnParaDisplay = (valor) => {
+        display.value += valor;
+    };
 
-        btnParaDisplay(valor) {
-            this.display.value += valor;
-        },
+    const clearDisplay = () => {
+        display.value = '';
+    };
 
-        clearDisplay() {
-            this.display.value = '';
-        },
+    const focusDisplay = () => {
+        display.focus();
+    };
 
-        focusDisplay() {
-            this.display.focus();
-        },
+    const apagaUm = () => {
+        display.value = display.value.slice(0, -1);
+    };
 
-        apagaUm() {
-            this.display.value = this.display.value.slice(0, -1);
-        },
+    const realizaConta = () => {
+        let conta = display.value;
 
-        realizaConta() {
-            let conta = this.display.value;
+        try {
+            conta = eval(conta);
 
-            try {
-                conta = eval(conta);
-
-                if (conta === undefined) {
-                    alert('Nenhum valor passado...');
-                    return;
-                }
-
-                if (conta === Infinity) {
-                    alert('Não é possível dividir por zero');
-
-                    this.clearDisplay();
-                    this.focusDisplay();
-                    
-                    return;
-                }
-
-                this.display.value = String(conta);
-            } catch (error) {
-                alert('Conta inválida');
-                this.clearDisplay();
-                this.focusDisplay();
+            if (conta === undefined) {
+                alert('Nenhum valor passado...');
                 return;
             }
 
-        },
+            if (conta === Infinity) {
+                alert('Não é possível dividir por zero');
 
-        cliqueBotoes() {
-            document.addEventListener('click', (event) => {
-                const element = event.target;
+                clearDisplay();
+                focusDisplay();
 
-                if (element.classList.contains('btn-num')) {
-                    this.btnParaDisplay(element.innerText);
-                }
+                return;
+            }
 
-                if (element.classList.contains('btn-clear')) {
-                    this.clearDisplay();
-                    this.focusDisplay();
-                }
+            display.value = String(conta);
+        } catch (error) {
+            alert('Conta inválida');
+            clearDisplay();
+            focusDisplay();
 
-                if (element.classList.contains('btn-del')) {
-                    this.apagaUm();
-                }
-
-                if (element.classList.contains('btn-eq')) {
-                    this.realizaConta();
-                }
-
-                if (element.classList.contains('btn-like')) {
-                    alert('Obrigado pelo like no projeto!');
-                    this.focusDisplay();    
-                }
-            });
-        },
-
-        pressionaEnter() {
-            this.display.addEventListener('keyup', (event) => {
-
-                if (event.keyCode === 13) {
-                    this.realizaConta();
-                }
-            });
-        },
-
-        inicia() {
-            this.cliqueBotoes();
-            this.pressionaEnter();
-            this.focusDisplay();
+            return;
         }
+    };
+
+    const cliqueBotoes = () => {
+        document.addEventListener('click', (event) => {
+            const element = event.target;
+
+            if (element.classList.contains('btn-num')) {
+                btnParaDisplay(element.innerText);
+            }
+
+            if (element.classList.contains('btn-clear')) {
+                clearDisplay();
+                focusDisplay();
+            }
+
+            if (element.classList.contains('btn-del')) {
+                apagaUm();
+            }
+
+            if (element.classList.contains('btn-eq')) {
+                realizaConta();
+            }
+
+            if (element.classList.contains('btn-like')) {
+                alert('Obrigado pelo like no projeto');
+                focusDisplay();
+            }
+        });
+    };
+
+    const pressionaEnter = () => {
+        display.addEventListener('keyup', (event) => {
+
+            if (event.keyCode === 13) {
+                realizaConta();
+            }
+        });
+    };
+
+
+    this.inicia = () => {
+        cliqueBotoes();
+        pressionaEnter();
+        focusDisplay();
     };
 }
 
-const calculadora = criaCalculadora();
+const calculadora = new Calculadora();
 calculadora.inicia();
